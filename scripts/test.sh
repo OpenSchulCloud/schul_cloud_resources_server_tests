49b0d2da2eb7035b56aa9d622bfad86b0672c026
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-05-02 05:29:57 -0700

    calling the tests with authentication

---
+++ scripts/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pytest --pyargs schul_cloud_ressources_server_tests     \
+       --basic=valid1@schul-cloud.org:123abc            \
+       --basic=valid2@schul-cloud.org:supersecure       \
+       --apikey=valid1@schul-cloud.org:abcdefghijklmn   \
+       --noauth=true
