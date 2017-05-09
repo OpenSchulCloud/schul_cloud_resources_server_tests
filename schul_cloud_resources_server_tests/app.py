91da3c1be44a82a5784e564359add8ae651c6a85
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-05-09 11:51:11 -0700

    remove encoding error for python 2
    https://travis-ci.org/schul-cloud/schul_cloud_resources_server_tests/jobs/230471329#L9999

--- schul_cloud_resources_server_tests/app.py
+++ schul_cloud_resources_server_tests/app.py
@@ -183,14 +183,14 @@ def add_resource():
     except ValidationFailed as error:
         abort(422, str(error))
     _id = add_request["data"].get("id", get_id())
-    link = get_location_url(_id)
     if not isinstance(_id, str) or not re.match("^([!*\"'(),+a-zA-Z0-9$_@.&+-]|%[0-9a-fA-F]{2})+$", _id):
         abort(403, "The id \"{}\" is invalid, can not be part of a url.".format(_id))
     if _id in resources or _id == "ids":
         abort(403, "The id \"{}\" already exists.".format(_id))
     resources[_id] = resource
-    response.headers["Location"] = link
     response.status = 201
+    link = get_location_url(_id)
+    response.headers["Location"] = link
     return response_object({"data": {"attributes": resource, "type":"resource", "id": _id},
             "links": {"self":link}})
 
