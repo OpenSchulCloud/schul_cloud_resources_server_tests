a8fd28abe32962efd1c82991bd459a525bfe8808
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-05-14 07:36:12 -0700

    using the test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python --version
-python -m schul_cloud_resources_server_tests.tests     \
+pytest --version
+pytest schul_cloud_resources_server_tests/tests         \
        --basic=valid1@schul-cloud.org:123abc            \
        --basic=valid2@schul-cloud.org:supersecure       \
        --apikey=valid1@schul-cloud.org:abcdefghijklmn   \
