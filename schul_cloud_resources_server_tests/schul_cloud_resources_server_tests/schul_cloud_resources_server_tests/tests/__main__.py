7f0903935e5dd7190f6d63554fca1f9758a5cce7
Extremely Penguin <extremelypenguin@localhost.localdomain>
2018-01-24 20:47:32 -0500

    deleted unnessescary files added by accident

--- schul_cloud_resources_server_tests/schul_cloud_resources_server_tests/schul_cloud_resources_server_tests/tests/__main__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import pytest
-
-HERE = os.path.dirname(__file__)
-API_TESTS = os.path.join(HERE, "test_api.py")
-
-errcode = pytest.main([API_TESTS] + sys.argv[1:])
-sys.exit(errcode)
